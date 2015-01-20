d24d11d844e1667f0943ed286d4c3478ba88f733
Dave Hawthorne <davehawthorne@ieee.org>
2015-01-19 19:50:34 -0600

    Creation of a simple pump panel.
    The pumper tanker panel was getting unworkable.

---
+++ panel_layout.js
@@ -0,0 +1,265 @@
+"use strict";
+/*global svg widgets*/
+
+var
+    colour = {
+        inlet: "green",
+        outlet: "blue",
+        pump: "red",
+        control: "black",
+        panel: "grey",
+        redLamp: [255, 0, 0],
+        greenLamp: [0, 255, 0],
+        blueLamp: [0, 0, 255]
+    },
+
+    dim = {
+        valvePaint: 50,
+        valveWidth: 100,
+        pipe: 20,
+        gauge: 90,
+        gaugePaint: 100,
+        rpm: 70,
+        rpmPaint: 80,
+        knobWidth: 40,
+        pump: 50,
+        pumpOut: 30,
+        control: 10,
+        valveHeight: 250,
+        rBevel: 25,
+        rGlobe: 18,
+        lampSpacing: 60
+    },
+
+    x = {
+        iv: 100,
+        ig: 300,
+        pt: 350,
+        p: 500,
+        og: 700,
+        ov: 900,
+        lamps: 650,
+        right: 1200
+    },
+
+    y = {
+        gc: 100,
+        l: 300,
+        vt: 250,
+        vb: 350,
+        ptt: 400,
+        ptb: 500,
+        rpm: 450,
+        lamps: 400,
+        bottom: 600
+    },
+
+    style = {
+        pipe: function (colour) {
+            return "stroke: " + colour + "; stroke-width: " + dim.pipe;
+        },
+        valve: function (colour) {
+            return "stroke-linecap: round; stroke: " + colour + "; stroke-width: " + dim.valvePaint;
+        },
+        control: function () {
+            return "stroke: " + colour.control + "; stroke-width: " + dim.control;
+        }
+
+    };
+
+
+function paintPump() {
+    svg.create("circle", {
+        cx: x.p,
+        cy: y.l,
+        r: dim.pump,
+        fill: colour.pump
+    });
+    svg.create("rect", {
+        x: x.p,
+        y: y.l - dim.pump,
+        width: dim.pump * 1.2,
+        height: dim.pump * 0.8,
+        fill: colour.pump
+    });
+    svg.create("circle", {
+        cx: x.p,
+        cy: y.l,
+        r: dim.pump * 0.5,
+        style: "stroke: black; stroke-width: " + (dim.pump * 0.05) + "; fill: none"
+    });
+}
+
+
+function paintPanel() {
+    svg.create("rect", {
+        x: 0,
+        y: 0,
+        width: x.right,
+        height: y.bottom,
+        fill: colour.panel
+    });
+    svg.create("line", {
+        x1: x.iv,
+        x2: x.iv,
+        y1: y.vt,
+        y2: y.vb,
+        style: style.valve(colour.inlet)
+    });
+    svg.create("line", {
+        x1: x.iv,
+        x2: x.p,
+        y1: y.l,
+        y2: y.l,
+        style: "stroke: " + colour.inlet + "; stroke-width: " + dim.pipe
+    });
+    svg.create("line", {
+        x1: x.ig,
+        x2: x.ig,
+        y1: y.l,
+        y2: y.gc,
+        style: "stroke: " + colour.inlet + "; stroke-width: " + dim.pipe
+    });
+    svg.create("circle", {
+        cx: x.ig,
+        cy: y.gc,
+        r: dim.gaugePaint,
+        fill: colour.inlet
+    });
+    svg.create("path", {
+        d: svg.path([
+            'M', [x.p, y.l - dim.pumpOut],
+            [x.p + dim.pump + dim.pumpOut, y.l - dim.pumpOut],
+            [x.p + dim.pump + dim.pumpOut * 2, y.l],
+            [x.ov, y.l]
+        ]),
+        style: "fill: none; stroke: " + colour.outlet + "; stroke-width: " + dim.pipe
+    });
+    svg.create("line", {
+        x1: x.og,
+        x2: x.og,
+        y1: y.l,
+        y2: y.gc,
+        style: style.pipe(colour.outlet)
+    });
+    svg.create("circle", {
+        cx: x.og,
+        cy: y.gc,
+        r: dim.gaugePaint,
+        fill: colour.outlet
+    });
+    svg.create("line", {
+        x1: x.ov,
+        x2: x.ov,
+        y1: y.vt,
+        y2: y.vb,
+        style: style.valve(colour.outlet)
+    });
+    svg.create("line", {
+        x1: x.p,
+        x2: x.p,
+        y1: y.l,
+        y2: y.rpm,
+        style: style.control()
+    });
+    svg.create("line", {
+        x1: x.pt,
+        x2: x.p,
+        y1: y.rpm,
+        y2: y.rpm,
+        style: style.control()
+    });
+    svg.create("circle", {
+        cx: x.p,
+        cy: y.rpm,
+        r: dim.rpmPaint,
+        fill: colour.pump
+    });
+    paintPump();
+    svg.create("line", {
+        x1: x.pt,
+        x2: x.pt,
+        y1: y.ptt,
+        y2: y.ptb,
+        style: style.valve(colour.pump)
+    });
+}
+
+
+function dummy() {
+}
+
+function sideways(xLoc) {
+    return (xLoc - x.right / 2) / 20;
+}
+
+
+function addLamp(lampNum, colour) {
+    return widgets.gauges.lamp({
+        cx: x.lamps,
+        cy: y.lamps + dim.lampSpacing * lampNum,
+        rBevel: dim.rBevel,
+        rGlobe: dim.rGlobe,
+        interval: 500,
+        colour: colour
+    });
+}
+
+
+function attachWidgets(de) {
+    return {
+        inValve: widgets.controls.outletValve({
+            yTop: y.vt,
+            yBot: y.vb,
+            knobWidth: dim.knobWidth,
+            width: dim.valveWidth,
+            height: dim.valveHeight,
+            callback: dummy,
+            cx: x.iv,
+            sideways: sideways(x.iv)
+        }),
+        combGauge: widgets.gauges.compound({
+            parent: de,
+            cx: x.ig,
+            cy: y.gc,
+            radius: dim.gauge
+        }),
+        revGauge: widgets.gauges.engineRevs({
+            parent: de,
+            cx: x.p,
+            cy: y.rpm,
+            radius: dim.rpm
+        }),
+        throttle: widgets.controls.outletValve({
+            yTop: y.ptt,
+            yBot: y.ptb,
+            knobWidth: dim.knobWidth,
+            width: dim.valveWidth,
+            height: dim.valveHeight,
+            callback: dummy,
+            cx: x.pt,
+            sideways: sideways(x.pt)
+        }),
+        outGauge: widgets.gauges.outlet({
+            parent: de,
+            cx: x.og,
+            cy: y.gc,
+            radius: dim.gauge
+        }),
+        outValve: widgets.controls.outletValve({
+            yTop: y.vt,
+            yBot: y.vb,
+            knobWidth: dim.knobWidth,
+            width: dim.valveWidth,
+            height: dim.valveHeight,
+            callback: dummy,
+            cx: x.ov,
+            sideways: sideways(x.ov)
+        }),
+        cavitation: addLamp(0, colour.redLamp),
+        hoseCollapse: addLamp(1, colour.blueLamp),
+        modelFail: addLamp(2, colour.greenLamp)
+
+
+    };
+}
---
+++ simple_pump.js
@@ -0,0 +1,180 @@
+/*global widgets model modelComponents utils svg paintPanel attachWidgets */
+"use strict";
+
+///
+/// Notes:
+///   All pressures are relative to atmospheric: 100kPa
+///   Pressures given in kPa, flows in l/min
+
+
+
+
+
+var handleException = function (ex) {
+    var
+    msg = "EX " + typeof ex + ' "' + ex.message + '" ' + ex.fileName + ':' + ex.lineNumber + "\n" + ex.stack;
+    alert(msg);
+};
+
+
+var svgDocument;
+var svgns = "http://www.w3.org/2000/svg";
+var xlinkNS = "http://www.w3.org/1999/xlink";
+
+
+var
+    panelItems,
+    engine,
+    flow = {},
+    engineTick,
+    attackLineFricLoss,
+    supplyLineFricLoss,
+    boost,
+    attackLine, outValve, inValve, pres, hydraulicTick, iterateFlow,
+    hydrantParams;
+
+attackLineFricLoss = modelComponents.hose.fricLoss.curry(modelComponents.hose.resistance.h38, 1, 1);
+
+boost = modelComponents.gaam.mk450.normalPressureBoost;
+
+outValve = modelComponents.valve(500, 500);
+attackLine = modelComponents.attackLine();
+inValve = modelComponents.valve(500, 1000); // inlet at halfway 1kL/min flow drops 500kPa through valve
+
+pres = {
+    hydrant: 300.0, //kPa
+    pumpEye: 0,
+    pumpOut: 0,
+    pumpHp: 0
+};
+
+engine = {
+    rpm: 800,
+    revInc: 0
+};
+
+flow.total = 0;
+
+
+
+/// run every tick interval, checks if operator is adjusting throttle
+//TEMP!!! the revs are just guesses
+engineTick = function () {
+};
+
+
+// Called once each tick.
+//
+//
+hydraulicTick = function () {
+    var
+        outlet,
+        total = 0.0,
+        inRes;
+
+    if (inValve.closed()) {
+        panelItems.modelFail.pulse();
+
+        // there's no source water, if any outlet is open drop the pressure to 0
+    } else {
+        inRes = inValve.resistance() + modelComponents.hose.resistance.h65 * 3;
+        pres.pumpEye = pres.hydrant - utils.sq(flow.total) * inRes;
+
+        if (pres.pumpEye < -100) {
+            panelItems.cavitation.pulse();
+        }
+        pres.pumpOut = pres.pumpEye + boost(engine.rpm, flow.total);
+
+
+    }
+
+
+    total = attackLine.getFlow(pres.pumpOut);
+
+    flow.total = (flow.total + total) / 2;
+
+    if (isNaN(flow.total)) {
+        //TEMP!!!
+        flow.total = 0;
+    }
+
+};
+
+
+
+function setOutValve(value) {
+    var totalRes;
+    outValve.set(value);
+    totalRes = outValve.resistance() + 1 / 650;    //TEMP!!! add a length of 38
+    attackLine.setHoseResistance(totalRes);
+}
+
+function setInValve(value) {
+    inValve.set(value);
+}
+
+
+function setThrottle(level) {
+    engine.rpm = level * 5000;
+    panelItems.modelFail.set(engine.rpm > 4000);
+}
+
+
+function tick() {
+    try {
+
+        engineTick();
+        hydraulicTick();
+    }
+    catch (ex) {
+        handleException(ex);
+    }
+
+}
+
+
+
+
+
+var updatePanel = function () {
+    var i;
+    panelItems.outGauge.showPressure(pres.pumpOut);
+    panelItems.combGauge.showPressure(pres.pumpEye);
+    panelItems.revGauge.showPressure(engine.rpm);
+    panelItems.outFlow.set(flow.total);
+};
+
+
+var buildPumpPanel = function (jqSvg, svgDocument) {
+    svg.setDocument(svgDocument);
+    try {
+        var
+            de = svgDocument.documentElement,
+            heart = utils.timerHeart(10);  // ten times per second
+
+        paintPanel();
+
+        panelItems = attachWidgets(de);
+
+        panelItems.outValve.setCallback(setOutValve);
+        panelItems.inValve.setCallback(setInValve);
+        panelItems.throttle.setCallback(setThrottle);
+
+        heart.addCallback(tick);
+        heart.addCallback(updatePanel);
+
+        heart.start();
+
+        attackLine.waterOn(true);
+
+    }
+    catch (ex) {
+        handleException(ex);
+    }
+};
+
+
+
+
+
+
